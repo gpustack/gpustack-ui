9d654bcd666d786933a329546fc43504f0382693
jialin <jialinkuang@126.com>
2024-10-31 11:05:12 +0800

    fix: show upgrade check

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -181,7 +181,9 @@ export default (props: any) => {
     return (
       initialState?.currentUser?.is_admin &&
       updateCheck.latest_version &&
-      updateCheck.latest_version !== version?.version
+      updateCheck.latest_version !== version?.version &&
+      updateCheck.latest_version !== '0.0.0' &&
+      updateCheck.latest_version?.indexOf('rc') === -1
     );
   }, [updateCheck, version, initialState]);
 

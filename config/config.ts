273a9a5e66424e39622602201f817328781744cc
jialin <jialinkuang@126.com>
2024-12-04 20:36:07 +0800

    fix: convert recording to mp3

--- config/config.ts
+++ config/config.ts
@@ -14,7 +14,7 @@ const isProduction = env === 'production';
 const t = Date.now();
 export default defineConfig({
   proxy: {
-    ...proxy()
+    ...proxy('http://192.168.50.4')
   },
   history: {
     type: 'hash'

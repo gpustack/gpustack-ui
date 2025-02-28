abcd3a30cef74d1d8ee091a0d38108fdbef12e42
rjohny55 <168018528+rjohny55@users.noreply.github.com>
2025-03-01 03:19:54 +0600

    Create apikeys.ts
    add ru_RU

---
+++ src/locales/ru-RU/apikeys.ts
@@ -0,0 +1,15 @@
+export default {
+  'apikeys.title': 'API-ключи',
+  'apikeys.table.apikeys': 'Ключи',
+  'apikeys.button.create': 'Создать API-ключ',
+  'apikeys.title.save': 'Сохранить API-ключ',
+  'apikeys.form.expiretime': 'Срок действия',
+  'apikeys.form.apikey': 'API-ключ',
+  'apikeys.table.name': 'Название ключа',
+  'apikeys.table.save.tips':
+    'Обязательно скопируйте ключ сразу. Вы не сможете увидеть его снова.',
+  'apikeys.form.expiration.7days': '7 дней',
+  'apikeys.form.expiration.1month': '1 месяц',
+  'apikeys.form.expiration.6months': '6 месяцев',
+  'apikeys.form.expiration.never': 'Без срока действия'
+};

bdb803db21643c6afa4dcc3a36e1a41c878ffc72
rjohny55 <168018528+rjohny55@users.noreply.github.com>
2025-03-01 03:48:15 +0600

    Create menu.ts

---
+++ src/locales/ru-RU/menu.ts
@@ -0,0 +1,21 @@
+export default {
+  'menu.dashboard': 'Панель управления',
+  'menu.playground': 'Песочница',
+  'menu.playground.rerank': 'Переранжирование',
+  'menu.playground.embedding': 'Векторизация',
+  'menu.playground.chat': 'Чат',
+  'menu.playground.speech': 'Аудио',
+  'menu.playground.text2images': 'Генерация изображений',
+  'menu.compare': 'Сравнение',
+  'menu.models': 'Модели',
+  'menu.models.modelList': 'Развертывание и управление',
+  'menu.models.modelCatalog': 'Каталог',
+  'menu.models.catalog': 'Каталог моделей',
+  'menu.modelCatalog': 'Каталог',
+  'menu.resources': 'Ресурсы',
+  'menu.apikeys': 'API-ключи',
+  'menu.users': 'Пользователи',
+  'menu.profile': 'Профиль',
+  'menu.login': 'Авторизация',
+  'menu.usage': 'Использование'
+};

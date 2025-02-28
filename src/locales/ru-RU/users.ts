6e4264704038962c4be61a595379dd7540b8ffa0
rjohny55 <168018528+rjohny55@users.noreply.github.com>
2025-03-01 03:53:28 +0600

    Create users.ts

---
+++ src/locales/ru-RU/users.ts
@@ -0,0 +1,31 @@
+export default {
+  'users.title': 'Пользователи',
+  'users.button.create': 'Новый пользователь',
+  'users.form.edit': 'Редактировать пользователя',
+  'users.form.create': 'Новый пользователь',
+  'users.table.username': 'Имя пользователя',
+  'users.table.role': 'Роль',
+  'users.form.fullname': 'Полное имя',
+  'users.table.user': 'пользователи',
+  'users.form.admin': 'Администратор',
+  'users.form.user': 'Пользователь',
+  'users.form.newpassword': 'Новый пароль',
+  'users.form.currentpassword': 'Текущий пароль',
+  'users.form.updatepassword': 'Изменить пароль',
+  'users.form.rule.password':
+    'Должен содержать заглавные и строчные буквы, цифры и спецсимволы (!@#$%^&*_+), длина 6-12 символов, без пробелов.',
+  'users.password.uppcase': 'Минимум одна заглавная буква',
+  'users.password.lowercase': 'Минимум одна строчная буква',
+  'users.password.number': 'Минимум одна цифра',
+  'users.password.special': 'Минимум один спецсимвол',
+  'users.password.length': 'Длина от 6 до 12 символов',
+  'users.password.modify.title': 'Смена пароля',
+  'users.password.modify.description':
+    'В целях безопасности измените первоначальный пароль.',
+  'users.password.confirm': 'Подтвердите новый пароль',
+  'users.password.confirm.empty': 'Подтвердите новый пароль',
+  'users.password.confirm.error': 'Пароли не совпадают',
+  'users.login.title': 'Вход в',
+  'users.version.islatest': 'GPUStack {version} — последняя версия',
+  'users.version.update': 'Доступно обновление GPUStack {version}'
+};

14418240060e85ed3a7d58b8182e4270e3786a4e
rjohny55 <168018528+rjohny55@users.noreply.github.com>
2025-03-01 03:51:49 +0600

    Create shortcuts.ts

---
+++ src/locales/ru-RU/shortcuts.ts
@@ -0,0 +1,13 @@
+export default {
+  'shortcuts.search.placeholder': 'Поиск сочетаний клавиш',
+  'shortcuts.title': 'Сочетания клавиш',
+  'shortcuts.playground.newmessage': 'Новое сообщение',
+  'shortcuts.playground.clearmessage': 'Очистить сообщения',
+  'shortcuts.playground.toggleparams': 'Свернуть/развернуть параметры',
+  'shortcuts.models.newmodelHF': 'Развернуть модель Hugging Face',
+  'shortcuts.models.newmodelLM': 'Развернуть модель Ollama',
+  'shortcuts.models.search': 'Поиск моделей в Hugging Face',
+  'shortcuts.resources.addworker': 'Добавить воркер',
+  'shortcuts.apikeys.new': 'Создать API-ключ',
+  'shortcuts.users.new': 'Создать пользователя'
+};

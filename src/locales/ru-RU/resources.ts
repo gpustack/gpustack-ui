72e2f5a2b0e14288649eaf806b04a5181dc2a639
EVGENY M <168018528+rjohny55@users.noreply.github.com>
2025-04-01 15:37:43 +0600

    Update resources.ts
    update ru

--- src/locales/ru-RU/resources.ts
+++ src/locales/ru-RU/resources.ts
@@ -13,14 +13,10 @@ export default {
   'resources.form.enablePartialOffload': 'Разрешить оффлоуд на CPU',
   'resources.form.placementStrategy': 'Стратегия размещения',
   'resources.form.workerSelector': 'Селектор воркеров',
-  'resources.form.enableDistributedInferenceAcrossWorkers':
-    'Разрешить распределённый инференс между воркерами',
-  'resources.form.spread.tips':
-    'Равномерно распределяет ресурсы между воркерами. Может увеличить фрагментацию ресурсов на отдельных воркерах.',
-  'resources.form.binpack.tips':
-    'Максимизирует утилизацию ресурсов, уменьшая фрагментацию на воркерах/GPU.',
-  'resources.form.workerSelector.description':
-    'Система выбирает подходящие GPU/воркеры для развертывания моделей на основе меток.',
+  'resources.form.enableDistributedInferenceAcrossWorkers': 'Разрешить распределённый инференс между воркерами',
+  'resources.form.spread.tips': 'Равномерно распределяет ресурсы между воркерами. Может увеличить фрагментацию ресурсов на отдельных воркерах.',
+  'resources.form.binpack.tips': 'Максимизирует утилизацию ресурсов, уменьшая фрагментацию на воркерах/GPU.',
+  'resources.form.workerSelector.description': 'Система выбирает подходящие GPU/воркеры для развертывания моделей на основе меток.',
   'resources.table.ip': 'IP-адрес',
   'resources.table.cpu': 'CPU',
   'resources.table.memory': 'ОЗУ',
@@ -41,19 +37,14 @@ export default {
   'resources.table.wokers': 'воркеры',
   'resources.worker.linuxormaxos': 'Linux или macOS',
   'resources.table.unified': 'Объединённая память',
-  'resources.worker.add.step1':
-    'Get Token <span class="note-text">(Run on the server)</span>',
+  'resources.worker.add.step1': 'Получить токен <span class="note-text">(выполнить на сервере)</span>',
   'resources.worker.add.step2': 'Зарегистрировать воркер',
-  'resources.worker.add.step2.tips':
-    'Note: run on the worker to be added, <span style="color: #000;font-weight: 600">mytoken</span> is the token obtained in the first step.',
-  'resources.worker.add.step3':
-    'После успешной регистрации обновите список воркеров.',
+  'resources.worker.add.step2.tips': 'Примечание: выполнить на добавляемом воркере, <span style="color: #000;font-weight: 600">mytoken</span> токен, полученный на первом шаге.',
+  'resources.worker.add.step3': 'После успешной регистрации обновите список воркеров.',
   'resources.worker.container.supported': 'Только для Linux.',
   'resources.worker.current.version': 'Текущая версия: {version}',
-  'resources.worker.driver.install':
-    'Установите необходимые драйверы и библиотеки перед установкой GPUStack.',
-  'resources.worker.select.command':
-    'Выберите метку для генерации команды и скопируйте её.',
+  'resources.worker.driver.install': 'Установите необходимые драйверы и библиотеки перед установкой GPUStack.',
+  'resources.worker.select.command': 'Выберите метку для генерации команды и скопируйте её.',
   'resources.worker.script.install': 'Установка скриптом',
   'resources.worker.container.install': 'Установка контейнером (только Linux)',
   'resources.worker.cann.tips': `Укажите индексы GPU через <span style='color: #000;font-weight: 600'>ASCEND_VISIBLE_DEVICES=0,1,2,3</span> или <span style='color: #000;font-weight: 600'>ASCEND_VISIBLE_DEVICES=0-3</span>.`,
@@ -63,17 +54,14 @@ export default {
   'resources.modelfiles.size': 'Размер',
   'resources.modelfiles.selecttarget': 'Выбрать назначение',
   'resources.modelfiles.form.localdir': 'Локальный каталог',
-  'resources.modelfiles.form.localdir.tips':
-    'Каталог по умолчанию <span class="desc-block>/var/lib/gpustack/cache</span> или каталог, указанный через <span class="desc-block">--data-dir</span>.',
+  'resources.modelfiles.form.localdir.tips': 'Каталог по умолчанию <span class="desc-block>/var/lib/gpustack/cache</span> или каталог, указанный через <span class="desc-block">--data-dir</span>.',
   'resources.modelfiles.retry.download': 'Повторить загрузку',
   'resources.modelfiles.storagePath.holder': 'Ожидание завершения загрузки...',
   'resources.filter.worker': 'Фильтровать по узлу',
   'resources.filter.source': 'Фильтровать по источнику',
-  'resources.modelfiles.delete.tips': 'Also delete the file from disk!'
+  'resources.modelfiles.delete.tips': 'Также удалить файл с диска!'
 };
 
 // ========== To-Do: Translate Keys (Remove After Translation) ==========
-// 1. 'resources.modelfiles.delete.tips',
-// 2. 'resources.worker.add.step1',
-// 3. 'resources.worker.add.step2.tips
+
 // ========== End of To-Do List ==========

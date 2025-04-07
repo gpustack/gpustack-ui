f7c597d9574e93cf54544e5cd36e9fd4efe8584c
jialin <jialinkuang@126.com>
2025-04-07 13:37:41 +0800

    style: shortcut for start to search model

--- src/locales/zh-CN/models.ts
+++ src/locales/zh-CN/models.ts
@@ -23,7 +23,7 @@ export default {
   'model.form.ollama.model': 'Ollama 模型',
   'model.form.ollamaholder': '请选择或输入模型名称',
   'model.deploy.sort': '排序',
-  'model.deploy.search.placeholder': '从 {source} 搜索模型',
+  'model.deploy.search.placeholder': '按 <kbd>/</kbd> 开始从 {source} 搜索模型',
   'model.form.ollamatips':
     '提示：以下为 GPUStack 预设的 Ollama 模型，请选择你想要的模型或者直接在右侧表单 【{name}】 输入框中输入你要部署的模型。',
   'models.sort.name': '名称',

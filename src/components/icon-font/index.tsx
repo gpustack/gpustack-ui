a4166864edb747ff1f16c27c8dc6671495abd947
jialin <jialinkuang@126.com>
2025-05-08 17:14:43 +0800

    chore: update iconfont files

--- src/components/icon-font/index.tsx
+++ src/components/icon-font/index.tsx
@@ -1,8 +1,8 @@
 import { createFromIconfontCN } from '@ant-design/icons';
-// import './iconfont/iconfont.js';
+import './iconfont/iconfont.js';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_4613488_dy3zc0nrfgk.js'
+  scriptUrl: ''
 });
 
 export default IconFont;

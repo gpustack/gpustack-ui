894dd1e353520309bcd90734950dc45304c55cfd
jialin <jialinkuang@126.com>
2025-05-27 14:27:21 +0800

    chore: update mindie parameters

--- src/pages/llmodels/config/mindie-config.ts
+++ src/pages/llmodels/config/mindie-config.ts
@@ -2,7 +2,7 @@ const options = [
   {
     label: '--log-level',
     value: '--log-level',
-    options: ['Verbose', 'Info', 'Warning', 'Warn', 'Error', 'Debug']
+    options: ['Info', 'Warn', 'Error', 'Debug']
   },
   {
     label: '--max-seq-len',
@@ -102,6 +102,75 @@ const options = [
   {
     label: '--override-generation-config',
     value: '--override-generation-config'
+  },
+  {
+    label: '--enable-split',
+    value: '--enable-split'
+  },
+  {
+    label: '--policy-type',
+    value: '--policy-type',
+    options: [0, 4, 5, 6, 7]
+  },
+  {
+    label: '--split-chunk-tokens',
+    value: '--split-chunk-tokens'
+  },
+  {
+    label: '--split-start-batch-size',
+    value: '--split-start-batch-size'
+  },
+  {
+    label: '--enable-memory-decoding',
+    value: '--enable-memory-decoding'
+  },
+  {
+    label: '--memory-decoding-length',
+    value: '--memory-decoding-length'
+  },
+  {
+    label: '--memory-decoding-dynamic-algo',
+    value: '--memory-decoding-dynamic-algo'
+  },
+  {
+    label: '--enable-lookahead',
+    value: '--enable-lookahead'
+  },
+  {
+    label: '--lookahead-level',
+    value: '--lookahead-level'
+  },
+  {
+    label: '--lookahead-window',
+    value: '--lookahead-window'
+  },
+  {
+    label: '--lookahead-guess-set-size',
+    value: '--lookahead-guess-set-size'
+  },
+  {
+    label: '--tensor-parallel-size',
+    value: '--tensor-parallel-size'
+  },
+  {
+    label: '--enable-expert-parallel',
+    value: '--enable-expert-parallel'
+  },
+  {
+    label: '--data-parallel-size',
+    value: '--data-parallel-size'
+  },
+  {
+    label: '--enable-buffer-response',
+    value: '--enable-buffer-response'
+  },
+  {
+    label: '--prefill-expected-time-ms',
+    value: '--prefill-expected-time-ms'
+  },
+  {
+    label: '--decode-expected-time-ms',
+    value: '--decode-expected-time-ms'
   }
 ];
 

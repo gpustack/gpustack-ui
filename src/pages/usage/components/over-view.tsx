c9c9401b0be1a1bd45fe928528168826cf8f6e97
jialin <jialinkuang@126.com>
2025-09-16 11:22:25 +0800

    fix: provder disabled click in k8s

--- src/pages/usage/components/over-view.tsx
+++ src/pages/usage/components/over-view.tsx
@@ -15,7 +15,7 @@ const renderCardItem = (data: {
   const { label, value, bgColor } = data;
   return (
     <Card
-      bordered={false}
+      variant="borderless"
       style={{ background: bgColor }}
       className={styles['card-body']}
     >

1ec4a949b8de3b617c18b0f43adfbbb13ba933c4
EVGENY M <168018528+rjohny55@users.noreply.github.com>
2025-09-24 13:43:09 +0600

    UI: Update ru playground.ts

--- src/locales/ru-RU/playground.ts
+++ src/locales/ru-RU/playground.ts
@@ -149,15 +149,11 @@ export default {
     '1. После выделения области создание маски станет недоступным; поэтому сначала нарисуйте маску, а затем активируйте опцию. \n 2. После загрузки изображения маски создание новых масок невозможно.',
   'playground.model.noavailable.tips2':
     'Если нужная модель не отображается, убедитесь, что она запущена и ей присвоена правильная категория. Если категория указана неверно, её можно изменить вручную в настройках модели.',
-  'playground.rerank.query.validate': 'The query is required.',
-  'playground.image.generate.error':
-    'Something went wrong. The image could not be generated.',
-  'playground.uploadfile.sizeError':
-    'File size exceeds the limit. Maximum allowed: {size}.'
+  'playground.rerank.query.validate': 'Необходимо указать запрос.',
+  'playground.image.generate.error': 'Произошла ошибка. Не удалось сгенерировать изображение.',
+  'playground.uploadfile.sizeError': 'Размер файла превышает ограничение. Максимальный размер: {size}.'
 };
 
 // ========== To-Do: Translate Keys (Remove After Translation) ==========
-// 1. 'playground.rerank.query.validate': 'The query is required.'
-// 2.  'playground.image.generate.error': 'Something went wrong. The image could not be generated.',
-// 3. 'playground.uploadfile.sizeError': 'File size exceeds the limit. Maximum allowed: {size}.'
+
 // ========== End of To-Do List ==========

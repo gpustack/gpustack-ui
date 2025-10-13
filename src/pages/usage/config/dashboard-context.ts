68d6993acd96bb8de48d84684286c2dace7b92d2
jialin <jialinkuang@126.com>
2025-10-13 16:47:37 +0800

    chore: remove test dir

--- src/pages/usage/config/dashboard-context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import { DashboardProps } from './types';
-
-export const DashboardContext = createContext<
-  DashboardProps & { fetchData: () => Promise<void> }
->({} as DashboardProps & { fetchData: () => Promise<void> });
-
-export default DashboardContext;

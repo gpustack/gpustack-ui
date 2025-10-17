49c6bb71a192704aef2c0b9f7a9a2d4a4c078ac2
jialin <jialinkuang@126.com>
2025-10-17 10:14:11 +0800

    feat: identicon

---
+++ src/pages/_components/identicon.tsx
@@ -0,0 +1,26 @@
+import jdenticon from 'jdenticon';
+import React, { useEffect, useRef } from 'react';
+
+interface IdenticonProps {
+  value: string;
+  size: number;
+}
+
+const Identicon: React.FC<IdenticonProps> = ({
+  value = 'test',
+  size = 28
+}: IdenticonProps) => {
+  const icon = useRef<any>(null);
+
+  useEffect(() => {
+    jdenticon?.update(icon.current, 'value');
+  }, [value]);
+
+  return (
+    <>
+      <svg data-jdenticon-value={value} height={size} ref={icon} width={size} />
+    </>
+  );
+};
+
+export default Identicon;

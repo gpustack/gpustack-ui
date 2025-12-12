b5a4cb671fa34356060c972951e78149f323f399
jialin <jialinkuang@126.com>
2025-12-12 10:31:43 +0800

    feat: get initial password

--- src/locales/zh-CN/users.ts
+++ src/locales/zh-CN/users.ts
@@ -36,5 +36,7 @@ export default {
   'users.settings.title': '用户设置',
   'users.status.activate': '启用账户',
   'users.status.deactivate': '停用账户',
-  'users.status.inactiveAccount': '停用账户'
+  'users.status.inactiveAccount': '停用账户',
+  'users.login.getInitialPassword':
+    '在服务器端运行以下命令以获取初始管理员密码。'
 };

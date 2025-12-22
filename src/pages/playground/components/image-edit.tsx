3be6c8cb8ef451fd34e964b3ed1f2af5a8c0b966
jialin <jialinkuang@126.com>
2025-12-22 18:11:03 +0800

    fix: allow to enter seed

--- src/pages/playground/components/image-edit.tsx
+++ src/pages/playground/components/image-edit.tsx
@@ -166,7 +166,10 @@ const GroundImages: React.FC<MessageProps> = forwardRef((props, ref) => {
   const generateParams = () => {
     const params = {
       ..._.omitBy(finalParameters, (value: string) => !value),
-      seed: parameters.random_seed ? generateRandomNumber() : null,
+      seed:
+        parameters.seed || parameters.random_seed
+          ? generateRandomNumber()
+          : null,
       stream: false,
       prompt: currentPrompt
     };

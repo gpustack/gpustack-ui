d3a0c6a33044f2d17b3595798542537349fa7d26
jialin <jialinkuang@126.com>
2026-01-30 17:53:22 +0800

    style: arrow bivider

--- src/pages/llmodels/components/separator.tsx
+++ src/pages/llmodels/components/separator.tsx
@@ -2,14 +2,19 @@ import { Divider } from 'antd';
 import React from 'react';
 import '../style/separator.less';
 
-const Separator: React.FC<{ showArrow?: boolean }> = ({ showArrow = true }) => {
+const Separator: React.FC<{
+  showArrow?: boolean;
+  styles: {
+    arrow?: React.CSSProperties;
+  };
+}> = ({ showArrow = true, styles }) => {
   return (
     <div className="separator">
       <Divider
         orientation="vertical"
         style={{ height: 'calc(100vh - 89px)', marginInline: '0px' }}
       ></Divider>
-      {showArrow && <span className="shape"></span>}
+      {showArrow && <span className="shape" style={styles.arrow}></span>}
     </div>
   );
 };

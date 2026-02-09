ba11689029d183b28ed41ce5112364a91954b852
jialin <jialinkuang@126.com>
2026-02-09 19:42:35 +0800

    feat: filter form for table

--- src/pages/_components/filter-form.tsx
+++ src/pages/_components/filter-form.tsx
@@ -1,7 +1,7 @@
 import OverlayScroller from '@/components/overlay-scroller';
-import { FilterOutlined } from '@ant-design/icons';
+import { CloseCircleFilled, FilterOutlined } from '@ant-design/icons';
 import { useIntl } from '@umijs/max';
-import { Button, Form, Popover } from 'antd';
+import { Badge, Button, Form, Popover } from 'antd';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
@@ -26,6 +26,36 @@ const Container = styled.div`
   }
 `;
 
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  .badge {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+  }
+  .close-btn {
+    position: absolute;
+    display: none;
+    top: -4px;
+    right: -4px;
+    font-size: 12px;
+    border-radius: 50%;
+    color: var(--ant-color-text-quaternary);
+    background-color: var(--ant-color-bg-container);
+    &:hover {
+      color: var(--ant-color-text-tertiary);
+    }
+  }
+  &:hover {
+    .close-btn {
+      display: block;
+    }
+  }
+`;
+
 const FilterForm: React.FC<
   React.PropsWithChildren & {
     width?: number;
@@ -40,12 +70,12 @@ const FilterForm: React.FC<
   width = 300,
   contentHeight = 400,
   initialValues = {},
-  hasFilters = false,
   onClose,
   onValuesChange
 }) => {
   const intl = useIntl();
   const [open, setOpen] = useState(false);
+  const [hasFilters, setHasFilters] = useState(false);
   const [form] = Form.useForm();
 
   const handleToggle = () => {
@@ -60,11 +90,20 @@ const FilterForm: React.FC<
     form.resetFields(Object.keys(initialValues));
     onValuesChange?.({}, initialValues);
     setOpen(false);
+    setHasFilters(false);
   };
 
   const handleOnClose = () => {
     setOpen(false);
     onClose?.();
+  };
+
+  const handleOnValuesChange = (changedValues: any, allValues: any) => {
+    onValuesChange?.(changedValues, allValues);
+    const hasActiveFilters = Object.values(allValues).some(
+      (value) => value !== undefined && value !== null && value !== ''
+    );
+    setHasFilters(hasActiveFilters);
   };
 
   return (
@@ -84,7 +123,7 @@ const FilterForm: React.FC<
             }}
           >
             <Form
-              onValuesChange={onValuesChange}
+              onValuesChange={handleOnValuesChange}
               initialValues={initialValues}
               form={form}
             >
@@ -113,7 +152,27 @@ const FilterForm: React.FC<
         }
       }}
     >
-      <Button onClick={handleToggle} icon={<FilterOutlined />}></Button>
+      <ButtonWrapper>
+        <Button icon={<FilterOutlined />} onClick={handleToggle}></Button>
+        {hasFilters && (
+          <Badge
+            dot
+            color={'var(--ant-color-primary-text-hover)'}
+            className="badge"
+          />
+        )}
+        {hasFilters && (
+          <span className="close-btn">
+            <CloseCircleFilled
+              style={{ fontSize: 12 }}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleOnReset();
+              }}
+            />
+          </span>
+        )}
+      </ButtonWrapper>
     </Popover>
   );
 };
